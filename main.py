46d8f6fe47c0dee0d27abdd84bed54de17b4ecf2
eastrd <yjia285@student.monash.edu>
2017-07-15 12:20:38 +1000

    整体重构

--- main.py
+++ main.py
@@ -1,50 +1,34 @@
 import winsound
 from tones import *
+from notes import *
 
-def Play_key(freq, duration):
-    winsound.Beep(freq, duration)
-
-def FilterChunk(numberNotes):
-    # Convert shorthanded number notes into machine readable format
-    Note = []
+def Interpret(numberNotes):
+    # 将“简易音谱”转换成“音乐机器码”
+    # 音符，音符拍子，拍子延长符
+    # 音符拍子可以是-或者=，对应音符下面的单/双下划线
+    # 延长符可以是+对应附点，~对应一个横杠
+    ASM_Note = []
     for i, e in enumerate(numberNotes):
         if e in note.keys():
-            Note.append(e)
-        elif e == "+":
-            #附点音符，之前的音长 * 1.5
-            Note[-1] += "+"
+            #检测到是音符
+            ASM_Note.append([note[e]])
+        elif e in duration.keys():
+            #检测到是音长
+            ASM_Note[-1].append(duration[e])
+        elif e in extension.keys():
+            #检测到是音长延长符
+            ASM_Note[-1][1] += int(ASM_Note[-1][1] * extension[e])
         else:
-            #音长
-            Note[-1] += e
-    for i in range(len(Note)):
-        if len(Note[i]) == 1:
-            Note[i] = Note[i] + "."
-    return Note
+            #妈的出错了
+            print("Error:",e,"in index",i)
+            exit()
+    return ASM_Note
 
-def Play_Chunk(ASM_Notes):
+def Play(ASM_Notes):
+    # 将完整整理好的“音乐机器码”播放出来：
+    #   [音符, 音长], [音符， 音长]
     for freq, time in ASM_Notes:
-        #print(freq, time)
-        Play_key(note[freq], duration[time])
+        winsound.Beep(freq, time)
 
-def Play(Notes):
-    # Wrapper function
-    Play_Chunk(FilterChunk(Notes))
-
-#回梦游仙
-a = "c-x-cc-6-x z=7=6-7=6-5-33-5-z3-5-76-5-6 c-xc-x-cc-6-x z=7=6- -6=6-5-33-5-z3-5-x- -c=x-5-6  7  7  7  "
-
-#天空之城
-b = "6-7-z 7-zc7  36 5-6z5  4-3-4 3-4z3  -z-z-z-7 4-477  6-7-z 7-zc7  3-3-6 5-6z5  34z-7zxc-z- z-7-6756  z-x-c x-cbx  5-5-z 7-zcc   6-7-z7-z-xz 5-5 vcxzccccc  cn b cx-z- "
-
-#风之甬道
-c = "e-t-yy-u-te-t-yy-2-uu-2-3-3-3-5-4-3-2-1-uy-e-ue-t-yy-u-te-t-yy-2-uu-2-3-3-3-5-4-3-u-e-t-  e-t-yy-u-te-t-yy-2-uu-2-3-3-3-5-4-3-2-1-"
-
-#可惜不是你
-d = "1=3-2=3=3=5- -5-3=2=2- -1=1   - -5=6-5=6=6=z- -z-5=5=5-3-2-2-2  - -2=3-2=3=3=5- -"
-
-# Always with Me
-e = "1-2-3-1-5 3-2521-y-3 1-u uyu1-2-t12-3-44-3-2-1-2 1-2-3-1-5 3-252-2-1-y-yu-1-t tyu1-2-t12-3-44-3-2-1-1~ 3-4-55555-6-5-4-33333-4-3-2-111-u-yuu-1-22-3-2-3-2 3-4-55555-6-5-4-3333-4-3-2-1-u-yy-u-1-2-t12-3-2 2-2-1-1~    "
-f = "1-+2-3-+"
-
-Play(b)
-#FilterChunk(f)
+for row in AlwaysWithMe:
+    Play(Interpret(row))
