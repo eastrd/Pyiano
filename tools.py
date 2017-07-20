75e32a73400a18d90b6db394d49b71792ec97a1c
eastrd <yjia285@student.monash.edu>
2017-07-20 22:54:25 +1000

    Init: ID <--=---> [Music, Duration] mapping script

---
+++ tools.py
@@ -0,0 +1,33 @@
+from main import *
+from notes import *
+
+
+total = []
+for song in [AlwaysWithMe, CastleInTheSky]:
+    for row in song:
+        total.extend(Interpret(row))
+
+
+
+unique_note = [list(x) for x in set(tuple(x) for x in total)]
+
+def song2note(song):
+    # 将音符+时长 转换为 ID化
+    totalsong = []
+    for row in song:
+        totalsong.extend(Interpret(row))
+    for i, e in enumerate(totalsong):
+        if e in unique_note:
+            totalsong[i] = unique_note.index(e)
+    return totalsong
+
+def note2song(note):
+    # 将ID化的代号 转换回 音符+时长
+    song = []
+    for ele in note:
+        song.append(unique_note[ele])
+    return song
+
+if __name__=="__main__":
+    Play(note2song())
+    #print(len(unique_note))
